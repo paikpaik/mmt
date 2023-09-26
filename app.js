54246580517ffd5ed797ada09021b13fb6effccb
paikpaik <woduf9311@gmail.com>
2023-09-26 16:28:44 +0900

    Init: mmt 초기 셋업

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const nunjucks = require("nunjucks");
+const dotenv = require("dotenv");
+
+dotenv.config();
+// 라우터
+const apiRouter = require("./routes");
+const app = express();
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
+
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
+// 라우터
+app.use("/api", apiRouter);
+app.get("/", (req, res, next) => {
+  res.send("Hello mmt!");
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 는 잘못된 접근입니다 O.O`);
+  error.status = 404;
+  next(error);
+});
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기 중");
+});
